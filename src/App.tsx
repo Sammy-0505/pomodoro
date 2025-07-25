209ddb488d4cd2b3a6a85ca8f5a3c080813692c0
Samuel Salles <samuelsalles918@gmail.com>
2025-07-24 23:35:23 -0300

    typed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import './styles/theme.css';
 import './styles/global.css';
 import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
-  return <Heading />;
+  return (
+    <Heading>
+      Olá Mundo
+      <button>
+        <TimerIcon />
+      </button>
+    </Heading>
+  );
 }
